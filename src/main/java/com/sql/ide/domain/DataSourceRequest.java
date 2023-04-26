f7f1b298302c917bc2b2bf22efc4d5bc79663499
Karishma Tayal <karishma.tayal@nagarro.com>
2023-04-26 09:56:55 +0530

    Code added for creating a sample txt file

--- src/main/java/com/sql/ide/domain/DataSourceRequest.java
+++ src/main/java/com/sql/ide/domain/DataSourceRequest.java
@@ -3,14 +3,24 @@ package com.sql.ide.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Data
 @Component
 @NoArgsConstructor
 @AllArgsConstructor
-public class DataSourceRequest {
-
+public class DataSourceRequest implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
     private String username;
 
     private String password;
@@ -18,4 +28,36 @@ public class DataSourceRequest {
     private String url;
 
     private String driver;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
 }
--- src/main/java/com/sql/ide/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/sql/ide/services/impl/ConnectionServiceImpl.java
@@ -1,11 +1,29 @@
 package com.sql.ide.services.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sql.ide.domain.DataSourceRequest;
 import com.sql.ide.services.ConnectionService;
 import com.sql.ide.services.CryptoService;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.io.BufferedWriter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Service;
@@ -33,23 +51,65 @@ public class ConnectionServiceImpl implements ConnectionService {
 
         try{
             jdbcTemplate.execute("show tables");
-
             // txt file(userId.txt) exist for user in "sql_resource" folder ?
-                // create one
-                // create Json
-
-                //json fetch -> decrypt -> check already present
-                // -> (if not)append/add new connection -> encrypt -> save in txt
-                // -> TODO: Connection already exist Exception
-
+            // create one
+            // create Json
+            //json fetch -> decrypt -> check already present
+            // -> (if not)append/add new connection -> encrypt -> save in txt
+            // -> TODO: Connection already exist Exception
             //create txt file for user and save jdbcTemplate after encrypt:
+            
+            
+			String path = "C:\\sql_resource/".concat(dataSourceRequest.getUsername()).concat(".txt");
+			File file = new File(path);
+			if (file.exists()) {
+				FileReader reader = new FileReader(path);
+				byte[] jsonData = Files.readAllBytes(Paths.get(path));
 
-        }catch (Exception e){
-            // invalid details, return error
-        }
+				ObjectMapper objectMapper = new ObjectMapper();
+				List<DataSourceRequest> allUserDetails = objectMapper.readValue(jsonData,
+						new TypeReference<List<DataSourceRequest>>() {
+						});
+
+				logger.info(allUserDetails);
+				int count = 0;
+				for (DataSourceRequest userData : allUserDetails) {
+					if (userData.getPassword().equalsIgnoreCase(dataSourceRequest.getPassword())
+							&& userData.getDriver().equalsIgnoreCase(dataSourceRequest.getDriver())
+							&& userData.getUrl().equalsIgnoreCase(dataSourceRequest.getUrl())
+							&& userData.getUsername().equalsIgnoreCase(dataSourceRequest.getUsername())) {
+						count++;
+					}
+
+				}
+				if (count == 0) {
+					allUserDetails.add(dataSourceRequest);
+					ObjectMapper mapper = new ObjectMapper();
+					String allJson = mapper.writeValueAsString(allUserDetails);
+					FileWriter fw = new FileWriter(path);
+					fw.write(allJson);
+					fw.close();
+				}
+			} else {
+				file.createNewFile();
+				logger.info("File Created" + file.getName());
+				FileWriter fw = new FileWriter(file.getAbsoluteFile());
+				ObjectMapper mapper = new ObjectMapper();
+				ArrayList<DataSourceRequest> dataSourceRequestList = new ArrayList<DataSourceRequest>();
+				dataSourceRequestList.add(dataSourceRequest);
+				String allJson = mapper.writeValueAsString(dataSourceRequestList);
+				fw.write(allJson);
+				fw.close();
+			}
+
+		} catch (Exception e) {
+			// invalid details, return error
+		}
 
 
 
         return null;
     }
+    
+    
 }
